6a9736e788e03d821279c46be9d419845eea460d
Einzel <einzel0102@mail.ru>
2024-04-07 15:58:23 +0800

    "Задача №1 - создание просто окна."

--- JavaFX_Task01/src/main/java/Main.java
+++ JavaFX_Task01/src/main/java/Main.java
@@ -1,2 +1,41 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.text.Text;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        // Создание корневого контейнера BorderPane;
+        BorderPane root = new BorderPane();
+
+        // Создание метки Label с текстом "Hello";
+        Label text = new Label("Hello");
+
+        // Установка размера шрифта для метки с помощью класса Font;
+        text.setFont(new Font(108));
+
+        // Установка метки в центре корневого контейнера BorderPane;
+        root.setCenter(text);
+
+        // Создание сцены Scene с размерами 800x600 пикселей;
+        Scene scene = new Scene(root, 800, 600);
+
+        // Установка заголовка для окна Stage;
+        stage.setTitle("Hello window");
+
+        // Установка сцены в окно Stage;
+        stage.setScene(scene);
+
+        // Отображение окна Stage на экране;
+        stage.show();
+    }
 }
